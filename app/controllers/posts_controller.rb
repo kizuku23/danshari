513e8ef6c86e473f02d3d0273e7ecc2d10d236c0
vagrant <vagrant@localhost.localdomain>
2020-09-05 07:57:13 +0000

    [Add] controllers and views

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,19 @@
+class PostsController < ApplicationController
+  def top
+  end
+
+  def about
+  end
+
+  def index
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
+  get 'users/index'
+  get 'users/edit'
+  get 'users/show'
+  get 'relationships/follower'
+  get 'relationships/followed'
+  get 'posts/top'
+  get 'posts/about'
+  get 'posts/index'
+  get 'posts/new'
+  get 'posts/edit'
+  get 'posts/show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
