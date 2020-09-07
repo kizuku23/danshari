92e55b55eed040f0517576a9fd826e98b19ff233
vagrant <vagrant@localhost.localdomain>
2020-09-07 09:05:29 +0000

    [Update] layout top,show

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :authenticate_user, except: [:top, :about]
+  before_action :authenticate_user!, except: [:top, :about]
 
   def top
     @posts = Post.all
