d84c0f8659fb9af04ff3bad29edd0198cdcf7def
vagrant <vagrant@localhost.localdomain>
2020-09-08 05:43:38 +0000

    [Add] comment system

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -34,6 +34,8 @@ class PostsController < ApplicationController
 
   def show
     @post = Post.find(params[:id])
+    @post_comment = PostComment.new
+    @post_comments = @post.post_comments
   end
 
   def update
