e785b40fe61942e0417b72b434564fc7a67aacae
vagrant <vagrant@localhost.localdomain>
2020-09-11 15:45:14 +0000

    [Add] skippr

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,15 +12,41 @@
 //
 //= require rails-ujs
 //= require activestorage
-//= require turbolinks
+
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
 //= require jquery.jscroll.min.js
 
-// jscroll, kaminariでスクロール
+// オプションを指定してSkipprの実行
+//$(document).on('turbolinks:load', function() {
+$(document).ready(function () {
+  $("#theTarget").skippr({
+    // スライドショーの変化（"fade" or "slide"）
+    transition : 'fade',
+    // 変化にかかる時間（ミリ秒）
+    speed : 500,
+    // easingの種類
+    easing : 'easeOutQuart',
+    // ナビゲーションの形（"block" or "bubble"）
+    navType : 'block',
+    // 子要素の種類（"div" or "img"）
+    childrenElementType : 'div',
+    // ナビゲーション矢印の表示（trueで表示）
+    arrows : false,
+    // スライドショーの自動再生（falseで自動再生なし）
+    autoPlay : true,
+    // 自動再生時のスライド切替間隔（ミリ秒）
+    autoPlayDuration : 5000,
+    // キーボードの矢印キーによるスライド送りの設定（trueで有効）
+    keyboardOnAlways : true,
+    // 1枚目のスライド表示時に戻る矢印を表示するかどうか [false]:矢印を隠さない [true]:矢印を隠す
+    hidePrevious : false
+  });
+});
 
-$(document).on('turbolinks:load', function() {
+// jscroll, kaminariでスクロール
+//$(document).on('turbolinks:load', function() {
   $(function() {
     $('.post-grid').jscroll({
       contentSelector: '.post-grid',
@@ -28,4 +54,4 @@ $(document).on('turbolinks:load', function() {
       loadingHtml: 'Now Loading'
     });
   });
-});
+//});
