872e4966a2148eeada04a017a115455c3a8a02f4
vagrant <vagrant@localhost.localdomain>
2020-09-15 15:14:14 +0000

    [Update] layout top, about

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -25,7 +25,7 @@ $(document).ready(function () {
     // スライドショーの変化（"fade" or "slide"）
     transition : 'fade',
     // 変化にかかる時間（ミリ秒）
-    speed : 500,
+    speed : 1000,
     // easingの種類
     easing : 'easeOutQuart',
     // ナビゲーションの形（"block" or "bubble"）
@@ -37,16 +37,16 @@ $(document).ready(function () {
     // スライドショーの自動再生（falseで自動再生なし）
     autoPlay : true,
     // 自動再生時のスライド切替間隔（ミリ秒）
-    autoPlayDuration : 5000,
+    autoPlayDuration : 2500,
     // キーボードの矢印キーによるスライド送りの設定（trueで有効）
     keyboardOnAlways : true,
     // 1枚目のスライド表示時に戻る矢印を表示するかどうか [false]:矢印を隠さない [true]:矢印を隠す
-    hidePrevious : false
+    hidePrevious : true
   });
 });
 
 // jscroll, kaminariでスクロール
-//$(document).on('turbolinks:load', function() {
+$(document).on('turbolinks:load', function() {
   $(function() {
     $('.grid').jscroll({
       contentSelector: '.grid',
@@ -54,7 +54,7 @@ $(document).ready(function () {
       loadingHtml: 'now loading'
     });
   });
-//});
+});
 
 //フラッシュメッセージのフェードアウト
   $(function(){
