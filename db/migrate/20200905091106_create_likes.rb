a6e0856dca3b274a83d45c801d22b3a41f8f4dd7
vagrant <vagrant@localhost.localdomain>
2020-09-18 04:03:52 +0000

    [Fix] migration files

--- db/migrate/20200905091106_create_likes.rb
+++ db/migrate/20200905091106_create_likes.rb
@@ -2,7 +2,7 @@ class CreateLikes < ActiveRecord::Migration[5.2]
   def change
     create_table :likes do |t|
       t.references :user, type: :integer, foreign_key: true
-      t.references :post, type: :bigint, foreign_key: true
+      t.references :post, type: :integer, foreign_key: true
 
       t.timestamps
     end
--- db/migrate/20200905091200_create_post_comments.rb
+++ db/migrate/20200905091200_create_post_comments.rb
@@ -2,7 +2,7 @@ class CreatePostComments < ActiveRecord::Migration[5.2]
   def change
     create_table :post_comments do |t|
       t.references :user, type: :integer, foreign_key: true
-      t.references :post, type: :bigint, foreign_key: true
+      t.references :post, type: :integer, foreign_key: true
       t.text :comment
 
       t.timestamps
