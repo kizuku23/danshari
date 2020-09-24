6da466753e61e21285ecc88339fd5b8f4ace5495
vagrant <vagrant@localhost.localdomain>
2020-09-24 07:29:17 +0000

    [Update] refactoring

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,16 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_user, only: [:edit, :show, :update]
+  before_action :ensure_correct_user, only: [:edit, :update]
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def show
-    @user = User.find(params[:id])
-    @posts = @user.posts.order(id: 'DESC').page(params[:page]).per(9)
+    @posts = @user.posts.order(id: 'DESC').page(params[:page])
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       redirect_to user_path(@user), notice: '登録内容を更新しました。'
     else
@@ -30,7 +29,7 @@ class UsersController < ApplicationController
   def search
     @user_or_post = params[:option]
     if @user_or_post == "1"
-      @posts = Post.search(params[:search], @user_or_post).order(id: 'DESC').page(params[:page]).per(9)
+      @posts = Post.search(params[:search], @user_or_post).order(id: 'DESC').page(params[:page])
     else
       @users = User.search(params[:search], @user_or_post)
     end
@@ -39,6 +38,19 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :introduction, :profile_image, :gender, :age, :status, :layout, :area)
+    params.require(:user).permit(
+      :name, :introduction, :profile_image, :gender,
+      :age, :status, :layout, :area,
+    )
+  end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def ensure_correct_user
+    unless @user == current_user
+      redirect_to user_path(current_user)
+    end
   end
 end
